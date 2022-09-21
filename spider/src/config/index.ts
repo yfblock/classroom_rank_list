28eebb851ba266e6860c0aee9a4fc4db5a13c417
yufeng <321353225@qq.com>
2022-09-21 08:21:42 +0800

    更新 spider信息添加测试

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+
+export const config = dotenv.config();
+
 export const organiztion: string = 'learningOS';
 export const assignments = ['lab0-0-setup-env-run-os1', 'lab1-os3'];
-export const AUTH_TOKEN = "";
+export const AUTH_TOKEN = process.env['AUTH_TOKEN'];
--- spider/src/index.ts
+++ spider/src/index.ts
@@ -1,9 +1,37 @@
 import { Octokit } from "octokit";
+import axios from "axios";
 import { assignments, AUTH_TOKEN } from "./config";
-
 const octokit = new Octokit({
     auth: AUTH_TOKEN
 })
+  
+
+async function fetchAssignments(classroom: string, assigment: string, sessionToken: string) {
+    console.log(sessionToken);
+    console.log(`https://classroom.github.com/classrooms/${classroom}/assignments/${assigment}/download_grades`);
+    return new Promise(async (resolve, reject) => {
+        const url = `https://classroom.github.com/classrooms/${classroom}/assignments/${assigment}/download_grades`
+        axios.defaults.headers.common = {
+            ...axios.defaults.headers.common,
+            accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'if-none-match': 'W/"91c8c819008d409c96ac22f96ff4029d"',
+            'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            cookie: `_github_classroom_session=${sessionToken}`
+        }
+        const response = await axios.get(url)
+        if(response.status) {
+            console.log(response.data)
+        }
+    })
+}
 
 async function test() {
     // let rate_limit = await octokit.request('GET /rate_limit', {})
@@ -29,19 +57,29 @@ async function test() {
     // })
     // console.log(log);
 
-    let log = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-        owner: 'os-autograding',
-        repo: 'oskernel-yfblock',
-        path: '2022_09_16_15_34_00.txt',
-        ref: 'gh-pages'
-    });
+    // let log = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    //     owner: 'os-autograding',
+    //     repo: 'oskernel-yfblock',
+    //     path: '2022_09_16_15_34_00.txt',
+    //     ref: 'gh-pages'
+    // });
     
-    let data = log.data['content' as keyof typeof log.data];
-    let encoding = log.data['encoding' as keyof typeof log.data];
-    let buff = Buffer.from(data, encoding);
-    let text = buff.toString('utf8');   
+    // let data = log.data['content' as keyof typeof log.data];
+    // let encoding = log.data['encoding' as keyof typeof log.data];
+    // let buff = Buffer.from(data, encoding);
+    // let text = buff.toString('utf8');   
 
-    console.log(text)
+    // console.log(text)
+    // console.log(process.env['ORG'])
+
+    let classroom = {
+        "name": "113154735-os-autograding-classroom-a857a2",
+        "assignments": ["oskernel"],
+        "studentBlacklist": []
+    };
+    let value = await fetchAssignments(classroom['name'], classroom['assignments'][0], process.env['SESSION_TOKEN'] ?? "");
+
+    console.log(value);
 }
 
 test()
