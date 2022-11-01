2c8f34b40e1ffc3af98a3190da79f1ee1337015a
yufeng <321353225@qq.com>
2022-11-01 15:11:17 +0800

    更新 配置文件

--- web-beta/next.config.js
+++ web-beta/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/rustlings_ranking/beta'
+  basePath: '/rustlings_ranking/beta',
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
